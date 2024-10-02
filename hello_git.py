4f49022902187eb07bba0c30c4ccd285a2dfefce
timsatlfalcons <timsatlfalcons@live.com>
2024-10-02 11:54:33 -0400

    changed  hello_git to function

--- hello_git.py
+++ hello_git.py
@@ -1,3 +1,6 @@
-print("Hello, Git!")
-print("Hello, everyone!")
-print("Goodbye")
+def style_text():
+    p1 = "Hello, Git!"
+    p2 = "Hello, everyone!"
+    p3 = "Goodbye"
+    return p1, p2, p3
+
